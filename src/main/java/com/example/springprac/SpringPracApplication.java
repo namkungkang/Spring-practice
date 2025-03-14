a01b97de015b4825e72e59c90ac36560869b473b
namkungkang <namkung0131@naver.com>
2025-03-14 10:41:41 +0900

    class,constructor문

--- src/main/java/com/example/springprac/SpringPracApplication.java
+++ src/main/java/com/example/springprac/SpringPracApplication.java
@@ -3,37 +3,42 @@ package com.example.springprac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class SpringPracApplication {
-   public static int plus() {
-         int r = 10;
-         int g = 20;
-         return r+g;
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringPracApplication.class, args);
-        int result = plus();
-        System.out.println(result);
 
-        final int a = 10;
-        System.out.println(a);
-
-        String b = "dsds";
-        b = "asaa";
-        System.out.println(b);
-
-        final int c = 1;
-        System.out.println(c);
-
-        if(3>1) {
-            System.out.println(true);
-        }
-
-        for(int i = 1; i<5; i++) {
-            System.out.println(i);
-        }
-
+        //class 문
+        var test = new Test(); //object
+        System.out.println(test.name);
+        test.hello();
+        //constructor 문
+        var fri = new Friend("남");
+        System.out.println(fri.name);
 
     }
 
 }
+
+class Test {
+    String name = "나"; //class 안의 변수는 filed 혹은 attribute
+
+    void hello() { //class 안의 함수는 method
+        System.out.println("안녕");
+    }
+}
+
+//constructor 문
+class Friend {
+    String name = "kim";
+    int age = 20;
+
+    Friend(String hole) {       //constructor(파라미터 여러 개 가능)
+        this.name = hole; // =<
+
+    }
+}
+
