1ba087cf1400cc994e6c950c0cf94c507a4e51f6
namkungkang <namkung0131@naver.com>
2025-03-15 11:17:39 +0900

    git-ignore 테스트

--- src/main/java/com/example/springprac/SpringPracApplication.java
+++ src/main/java/com/example/springprac/SpringPracApplication.java
@@ -9,36 +9,9 @@ import java.sql.SQLOutput;
 public class SpringPracApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SpringPracApplication.class, args);
-
-        //class 문
-        var test = new Test(); //object
-        System.out.println(test.name);
-        test.hello();
-        //constructor 문
-        var fri = new Friend("남");
-        System.out.println(fri.name);
-
-    }
-
 }
 
-class Test {
-    String name = "나"; //class 안의 변수는 filed 혹은 attribute
-
-    void hello() { //class 안의 함수는 method
-        System.out.println("안녕");
-    }
 }
-
-//constructor 문
-class Friend {
-    String name = "kim";
-    int age = 20;
-
-    Friend(String hole) {       //constructor(파라미터 여러 개 가능)
-        this.name = hole; // =<
-
-    }
-}
-
