26b01fa2e003f224335d4e4add2eac42a066a2c8
namkungkang <namkung0131@naver.com>
2025-03-16 20:53:11 +0900

    스프링 초기설정

--- src/main/java/com/example/springprac/BasicController.java
+++ src/main/java/com/example/springprac/BasicController.java
@@ -1,10 +1,18 @@
 package com.example.springprac;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
 public class BasicController {
-    public static void main(String[] args) {
-
-
-
+    @GetMapping
+    @ResponseBody
+    String hello() {
+        return "hello";
     }
 
 }
--- src/main/java/com/example/springprac/SpringPracApplication.java
+++ src/main/java/com/example/springprac/SpringPracApplication.java
@@ -10,7 +10,7 @@ public class SpringPracApplication {
 
     public static void main(String[] args) {
 
-
+        System.out.println("dfdd");
         SpringApplication.run(SpringPracApplication.class, args);
 }
 
